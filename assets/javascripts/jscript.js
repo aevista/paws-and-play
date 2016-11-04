62204d3a5e7c947666fa9510c585165c3e70b14c
aevista <aevistax@gmail.com>
2016-11-04 10:56:18 -0500

    responsive title and background

---
+++ assets/javascripts/jscript.js
@@ -0,0 +1,19 @@
+
+window.onresize = function() {
+    console.log("here");
+    var width = Math.max(document.documentElement.clientWidth, window.innerWidth || 0)
+    var elmt = document.getElementById("backgroundimage");
+
+
+    elmt.style.left = (width * 0.15).toString();
+    
+    if (width <= 1340) { elmt.style.left = "0"; }
+    /*else if (width <= 720) { elmt.style.left =  "100";}
+    else if (width <= 960) { elmt.style.left = "150px";}
+    else if (width <=1140){ elmt.style.left = "200px"; }
+    else elmtn.style.left = "400px";
+    */
+};
+
+window.onresize();
+
