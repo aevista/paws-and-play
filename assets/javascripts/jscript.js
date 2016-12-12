b1900813969699caf6c4b3f78af026f6a24ebb2b
aevista <aevistax@gmail.com>
2016-12-11 21:00:52 -0600

    design stuff added

--- assets/javascripts/jscript.js
+++ assets/javascripts/jscript.js
@@ -8,6 +8,13 @@ window.onresize = function() {
     elmt.style.left = (width * 0.15).toString();
     
     if (width <= 1340) { elmt.style.left = "0"; }
+    if (width <= 768) { 
+        var div = document.getElementById("content-body-title");
+        div.style.paddingTop = "0px";
+    }
+    else {
+        div.style.paddingTop = "25px";
+    }
     /*else if (width <= 720) { elmt.style.left =  "100";}
     else if (width <= 960) { elmt.style.left = "150px";}
     else if (width <=1140){ elmt.style.left = "200px"; }
