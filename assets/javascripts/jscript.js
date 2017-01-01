9e596f8218b4e14fff350558db95f598ada944cc
aevista <aevistax@gmail.com>
2017-01-01 12:29:13 -0600

    added https jquery script instead of http

--- assets/javascripts/jscript.js
+++ assets/javascripts/jscript.js
@@ -7,14 +7,6 @@ var onresize = function() {
     elmt.style.left = (width * 0.15).toString();
     
     if (width <= 1340) { elmt.style.left = "0"; }
-    width <= 768 ? content_body_title.style.paddingTop = "0px" : content_body_title.style.paddingTop = "20px";
-        
-    
-    /*else if (width <= 720) { elmt.style.left =  "100";}
-    else if (width <= 960) { elmt.style.left = "150px";}
-    else if (width <=1140){ elmt.style.left = "200px"; }
-    else elmtn.style.left = "400px";
-    */
 };
 
 $(document).ready(function() {
