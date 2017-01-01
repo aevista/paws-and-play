30c3cf212a504cd24b3cb6907a933153291c811c
aevista <aevistax@gmail.com>
2016-12-31 19:00:13 -0600

    added fancybox picture stuff, about page, favicon, header title, accessible links

---
+++ aboutus.md
@@ -0,0 +1,5 @@
+---
+layout: index
+title: About
+body: aboutbody.html
+---
--- assets/javascripts/jscript.js
+++ assets/javascripts/jscript.js
@@ -1,20 +1,15 @@
 
-window.onresize = function() {
-    console.log("here");
+var onresize = function() {
     var width = Math.max(document.documentElement.clientWidth, window.innerWidth || 0)
     var elmt = document.getElementById("backgroundimage");
-
+    var content_body_title = document.getElementById("content-body-title");
 
     elmt.style.left = (width * 0.15).toString();
     
     if (width <= 1340) { elmt.style.left = "0"; }
-    if (width <= 768) { 
-        var div = document.getElementById("content-body-title");
-        div.style.paddingTop = "0px";
-    }
-    else {
-        div.style.paddingTop = "25px";
-    }
+    width <= 768 ? content_body_title.style.paddingTop = "0px" : content_body_title.style.paddingTop = "20px";
+        
+    
     /*else if (width <= 720) { elmt.style.left =  "100";}
     else if (width <= 960) { elmt.style.left = "150px";}
     else if (width <=1140){ elmt.style.left = "200px"; }
@@ -22,5 +17,7 @@ window.onresize = function() {
     */
 };
 
-window.onresize();
-
+$(document).ready(function() {
+    $(".fancybox-thumb").fancybox();
+    $( window ).resize(onresize());
+});
